518364883f9e216529d70562d4aa01fcfbda417e
Calin Ilie <calin@192.168.1.7>
2025-09-21 19:18:16 +0300

    Analytics and SpeedInsights from Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 
 // font global pentru tot site-ul
 import './styles/global.css'
@@ -11,5 +13,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Analytics />
+    <SpeedInsights />
+  </React.StrictMode>
+);
